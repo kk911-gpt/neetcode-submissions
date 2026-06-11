fe2bd288efd3eceb3792871f29358ab255ff7762
kk911-gpt <courseskk911@gmail.com>
2026-06-11 15:27:32 +0530

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.py
@@ -0,0 +1,28 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+
+        n1= len(s1)
+        n2=len(s2)
+
+        if n1> n2 :
+            return False
+
+        count_1= [0]*26
+        count_2= [0]*26
+
+        for i in range (n1):
+
+            count_1[ord(s1[i])-97]+=1
+            count_2[ord(s2[i])-97]+=1
+
+        if count_1 == count_2:
+            return True
+        for i in range (n1,n2):
+            count_2[ord(s2[i])-97]+=1
+            count_2[ord(s2[i-n1])-97] -=1
+
+            if count_1==count_2:
+                return True
+        return False
+
+        
